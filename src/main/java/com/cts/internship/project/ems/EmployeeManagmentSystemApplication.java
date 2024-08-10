6d5083e98e456b7521837a17288e085400b7361d
veyyone <veyyonemuruganantham@gmail.com>
2024-08-10 22:00:06 +0530

    clear the errors

--- src/main/java/com/cts/internship/project/ems/EmployeeManagmentSystemApplication.java
+++ src/main/java/com/cts/internship/project/ems/EmployeeManagmentSystemApplication.java
@@ -2,7 +2,7 @@ package com.cts.internship.project.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication
